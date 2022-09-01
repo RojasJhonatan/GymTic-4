95fb7da2aac10623f429bdc5ce3be3b0e44749a7
karengv12 <111544071+karengv12@users.noreply.github.com>
2022-08-31 22:57:52 -0500

    Add files via upload

--- GymTic.App.Dominio/Entidades/Nutricion.cs
+++ GymTic.App.Dominio/Entidades/Nutricion.cs
@@ -1,10 +1,11 @@
 using system;
 
-namespace GymTic.App.Dominio{
-    public class Nutricion
+namespace Gimnasio.App.Dominio
+{
+   public class Nutricion{
+    public Dieta Dieta{get;set;}
+    public int PuntosVerificacion{get;set;}
+    
 
-        {
-        public Dieta Dieta{get;set;}
-        public int PuntosVerificacion{get;set;}
-        }
-    }
+   }
+}
