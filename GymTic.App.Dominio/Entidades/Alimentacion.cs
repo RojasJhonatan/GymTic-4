82da647d069956c8c016a6f229b67e567f0c50a8
Jose Sanchez <alejotri99@hotmail.com>
2022-08-31 22:24:43 -0500

    alimentacion23108

--- GymTic.App.Dominio/Entidades/Alimentacion.cs
+++ GymTic.App.Dominio/Entidades/Alimentacion.cs
@@ -1,1 +1,13 @@
-using system;
+using System;
+
+namespace Gimnasio.App.Dominio
+{
+    public class Alimentacion
+    {
+        public string Desayuno{get;set;}
+        public string Almuerzo{get;set;}
+        public string Cena{get;set;}
+        public string DiasSemana{get;set;}
+
+    }
+}
