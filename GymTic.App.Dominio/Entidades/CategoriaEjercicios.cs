bb72e8cf2014fe04ece328779f3acf9528492ec9
Arnol Garzon <arnol_garzon@hotmail.com>
2022-09-02 22:37:40 -0500

    Actualizacion entidades 02-9

--- GymTic.App.Dominio/Entidades/CategoriaEjercicios.cs
+++ GymTic.App.Dominio/Entidades/CategoriaEjercicios.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace GymTic.App.Dominio{
     public class CategoriaEjercicios{
+        public Nombre Nombre {get; set;}
+        public PuntosSaludables PuntosSaludables {get; set;}
+    
     }
-
 }
--- GymTic.App.Dominio/Entidades/Rutina.cs
+++ GymTic.App.Dominio/Entidades/Rutina.cs
@@ -2,6 +2,9 @@ using system;
 
 namespace GymTic.App.Dominio{
     public class Rutina{
+        public Ejercicios Ejercicios {get; set;}
+        public Horario Horario {get; set;}
+       
     }
 
 }
