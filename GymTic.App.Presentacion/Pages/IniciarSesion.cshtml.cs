d0fe65ff9288dd34683ad432b97e214d442510c0
Arnol Garzon <arnol_garzon@hotmail.com>
2022-09-10 08:20:20 -0500

    actulizacion index 10/09/2022

---
+++ GymTic.App.Presentacion/Pages/IniciarSesion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymTic.App.Presentacion.Pages
+{
+    public class IniciarSesionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
