4f41ce00dc82ac61c8495510f95f96e3401ce517
RojasJhonatan <jmr.85@hotmail.com>
2022-09-11 12:11:44 -0500

    persistenciarutina1109

--- GymTic.App.Persistencia/AppRepositorio/RepositorioDieta.cs
+++ GymTic.App.Persistencia/AppRepositorio/RepositorioDieta.cs
@@ -12,6 +12,53 @@ namespace Gymtic.App.Persistencia
             this._appContextBD = appContext;
         }
         
+        public Dieta CrearDieta(Dieta dieta)
+        {
+            var dietaCreado = _appContextBD.Dieta.Add(dieta);
+            _appContextBD.SaveChanges();
+            return dietaCreado.Entity;
+        }
+
+        public Dieta ConsultarDieta(string nombreDieta)
+        {
+            var dietaEncontrado = _appContextBD.Cliente.FirstOrDefault(d => d.nombre == nombreDieta);
+            if(dietaEncontrado !=null)
+            {
+                return dietaEncontrado;
+            }
+            return dietaEncontrado;
+        }
+
+        public IEnumerable<Dieta> ConsultarTodosDietas()
+        {
+            return _appContextBD.Dietas;
+        }
+
+        public Dieta actualizarDieta(Dieta dieta)
+        {
+            var dietaEncontrado = _appContextBD.Dieta.FirstOrDefault(d => d.nombre == dieta.Nombre);
+            if(dietaEncontrado != null)
+            {
+                dietaEncontrado.nombre = dieta.Nombre;
+                dietaEncontrado.alimentacion = dieta.alimentacion;
+                dietaEncontrado.duracion = dieta.duracion;
+                dietaEncontrado.categoria = dieta.categoria;
+                dietaEncontrado.verificacion = dieta.verificacion;
+
+                _appContextBD.SaveChanges();
+            }
+            return dietaEncontrado;
+
+            public void eliminarDieta(string nombre)
+            {
+                var dietaEncontrado = _appContextBD.Dieta.FirstOrDefault(d => d.nombre == dieta.Nombre);
+                if(dietaEncontrado == null)
+                return;
+                _appContextBD.Dieta.Remove(dietaEncontrado);
+                _appContextBD.SaveChanges();
+
+            }
+        }
 
     }
 }
