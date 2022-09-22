872c99a3e86a13eb55b5c9d4550b8e915528df68
RojasJhonatan <jmr.85@hotmail.com>
2022-09-21 21:51:29 -0500

    Página crearclienteJ2109

--- GymTic.App.Dominio/Entidades/Cliente.cs
+++ GymTic.App.Dominio/Entidades/Cliente.cs
@@ -1,18 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GymTic.App.Dominio
 {
     public class Cliente
     {
        public int id {get;set;}
-       public string nombre {get; set;} 
+       [Required]
+       public string nombre {get; set;}
+       [Required] 
        public string apellido {get;set;}
+       [Required]
        public string eMail {get;set;}
+       [Required]
        public string contraseña {get;set;}
+       [Required]
        public string telefono {get;set;}
        public string direccion {get;set;}
+       [Required]
        public float peso {get;set;}
+       [Required]
        public float altura {get;set;}
+       [Required]
        public Genero genero {get;set;}
     }
 
---
+++ GymTic.App.Frontend/Pages/CrearCliente/CrearCliente.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymTic.App.Frontend.Page
+{
+    public class CrearClienteModel : PageModel
+    {
+        //[BindProperty]
+        public void OnGet()
+        {
+        }
+    }
+}
