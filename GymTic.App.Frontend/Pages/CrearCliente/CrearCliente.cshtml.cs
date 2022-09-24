569df7d7f7b38f9643924f38593142e981e94d93
RojasJhonatan <jmr.85@hotmail.com>
2022-09-23 21:56:48 -0500

    Página crear cliente J2309

--- GymTic.App.Frontend/Pages/CrearCliente/CrearCliente.cshtml.cs
+++ GymTic.App.Frontend/Pages/CrearCliente/CrearCliente.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GymTic.App.Dominio;
 
 namespace GymTic.App.Frontend.Page
 {
     public class CrearClienteModel : PageModel
     {
+        public Cliente cliente{get;set;}
         //[BindProperty]
         public void OnGet()
         {
