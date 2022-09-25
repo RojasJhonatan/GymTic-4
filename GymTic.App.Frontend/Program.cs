5794e57d299cd5084099fb4c90eff8f39d3ecd77
RojasJhonatan <jmr.85@hotmail.com>
2022-09-25 09:02:56 -0500

    ConfiguracionProgram.csJ2509

--- GymTic.App.Frontend/Program.cs
+++ GymTic.App.Frontend/Program.cs
@@ -1,7 +1,16 @@
+
+using GymTic.App.Persistencia;
+using GymTic.App.Persistencia.AppRepositorio;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = new ServiceCollection();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.services.AddSingleton<IRepositorioCliente>();
+builder.services.AddSingleton<IRepositorioRutina, RepositorioRutina>();
+builder.services.AddSingleton<IRepositorioEjercicio, RepositorioEjercicio>()
 
 var app = builder.Build();
 
@@ -12,14 +21,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
+///
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
