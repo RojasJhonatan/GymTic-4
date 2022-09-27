b37bc9d4fac13a6849e2d84471b9669870d7b9c2
RojasJhonatan <jmr.85@hotmail.com>
2022-09-27 15:11:50 -0500

    EdicionYBuscarJ2709

---
+++ GymTic.App.Frontend/Pages/BuscarCliente/BuscarCliente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymTic.App.Frontend.Pages
+{
+    public class BuscarClienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
